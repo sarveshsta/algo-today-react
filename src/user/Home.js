11230fe6a4181cdd1b60338049337573386ad6a1
amanSTA123 <aman@seizetheads.com>
2024-02-16 12:47:56 +0530

    Added signup button in Home

--- src/user/Home.js
+++ src/user/Home.js
@@ -133,6 +133,7 @@ const Home = () => {
             {" "}
             Algotoday
           </h1>
+          <Link to="/signup"><button id="signup-button-home" type="button" class="btn btn-primary">Signup</button></Link>
         </div>
 
         <div
