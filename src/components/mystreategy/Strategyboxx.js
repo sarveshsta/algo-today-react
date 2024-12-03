37b5775b09660254118355b2e033d6e1c86e9b0c
amanSTA123 <aman@seizetheads.com>
2024-12-03 23:17:38 +0530

    stretegy data add

--- src/components/mystreategy/Strategyboxx.js
+++ src/components/mystreategy/Strategyboxx.js
@@ -1,18 +1,20 @@
- export function StrategyBox({ id, title, buttonText, onSubscribe, dataModal }) {
-    return (
-      <div className="subbox" id={id}>
-        <h2 className="boxHeading">{title}</h2>
-        <p className="boxpara">
-          This Strategy will help you to trade on your end, Each stretegy has unique parameters like RSI, index, Pivot are many more. This indicators and strategy parameters will help you to gain a good profit
-        </p>
-        <button
-          type="button"
-          className="btn btn-primary"
-          data-micromodal-trigger={dataModal}
-          onClick={() => onSubscribe()}
-        >
-          {buttonText}
-        </button>
-      </div>
-    );
-  };
+export function StrategyBox({ id, title, buttonText, onSubscribe, dataModal }) {
+  return (
+    <div className="subbox" id={id}>
+      <h2 className="boxHeading">{title}</h2>
+      <p className="boxpara">
+        This Strategy will help you to trade on your end, Each stretegy has
+        unique parameters like RSI, index, Pivot are many more. This indicators
+        and strategy parameters will help you to gain a good profit
+      </p>
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-micromodal-trigger={dataModal}
+        onClick={() => onSubscribe()}
+      >
+        {buttonText}
+      </button>
+    </div>
+  );
+}
